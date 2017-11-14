4521832fd2e6988290838fc24b52a0ce71289349
wailinaung <wailinaung@nexlabs.co>
2017-11-14 20:56:59 +0630

    includ code for assets folder

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,14 @@ const cleanCSS = require('gulp-clean-css');
 
 gulp.task('html', () => {
   gulp.src(['**/*.html', '!dist/**/*.html', '!./node_modules/**'])
-  .pipe(gulp.dest('dist/'))
-  .pipe(reload({stream: true}));
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({ stream: true }));
 });
+
+gulp.task('copy-assets', () => {
+  gulp.src(['assets/*'])
+    .pipe(gulp.dest('dist'));
+})
 
 gulp.task('browser-sync', () => {
   browserSync({
@@ -26,33 +31,34 @@ gulp.task('browser-sync', () => {
 
 gulp.task('sass', () => {
   gulp.src('scss/**/*.scss')
-  .pipe(plumber())
-  .pipe(autoprefixer())
-  .pipe(sass({style: 'expanded'}))
-  .on('error', gutil.log)
-  .pipe(gulp.dest('dist/css/'))
-  .pipe(reload({stream: true}));
+    .pipe(plumber())
+    .pipe(autoprefixer())
+    .pipe(sass({ style: 'expanded' }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(reload({ stream: true }));
 });
 
 gulp.task('minify-css', () => {
   gulp.src('dist/css/**/*.css')
-  .pipe(cleanCSS({debug: true, compatibility: 'ie8'}))
-  .pipe(gulp.dest('dist/css/'))
-  .pipe(reload({stream: true}));
+    .pipe(cleanCSS({ debug: true, compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(reload({ stream: true }));
 });
 
 gulp.task('js', () => {
   gulp.src('js/**/*.js')
-  .pipe(uglify())
-  .pipe(concat('script.js'))
-  .on('error', gutil.log)
-  .pipe(gulp.dest('dist/js/'));
+    .pipe(uglify())
+    .pipe(concat('script.js'))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('watch', () => {
   gulp.watch(['**/*.html', '!dist/**/*.html'], ['html']);
   gulp.watch('scss/**/*.scss', ['sass', 'minify-css']);
   gulp.watch('js/**/*.js', ['js']);
+  gulp.watch('assets/*', ['copy-assets']);
 });
 
-gulp.task('default', ['html', 'sass', 'minify-css', 'js', 'browser-sync', 'watch']);
+gulp.task('default', ['html', 'copy-assets', 'sass', 'minify-css', 'js', 'browser-sync', 'watch']);
